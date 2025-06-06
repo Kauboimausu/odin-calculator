c757d68694d1e009b22dc54d1c779f1624d095af
kauboimausu <malagonngasca@gmail.com>
2025-06-06 02:12:06 -0600

    Added a lot of html and css, mostly structured and looks how i want it to, but needs color and other tweaks As for the functioning I've only implemented the most rudimentary parts, but the implementation should be pretty easy, I think

---
+++ index.js
@@ -0,0 +1,34 @@
+// The global variables we'll use for evaluating expressions
+
+let left = 0;
+let right = 0
+let operand = null;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(operator, leftVariable, rightVariable){
+    switch (operator) {
+        case "+":
+            return add(leftVariable, rightVariable);
+        case "-":
+            return subtract(leftVariable, rightVariable);
+        case "x":
+            return multiply(leftVariable, rightVariable);
+        case "/":
+            return divide(leftVariable, rightVariable);
+    }
+}
