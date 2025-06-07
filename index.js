aacef727a2d14a42417a9766953a3ad138c8e7d2
kauboimausu <malagonngasca@gmail.com>
2025-06-07 02:37:15 -0600

    Added a lot of functionality and fixed some bugs, just need some more features and it's done

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // The global variables we'll use for evaluating expressions
 
-let left = 0;
-let right = 0
-let operand = null;
+let numbers = ["", ""];
+let numberIndex = 0;
+let operand = "";
+
+// The display element, we'll change the text dynamically to correspond to buttons pressed
+
+let display = document.querySelector(".display-text");
 
 function add(a, b){
     return a + b;
@@ -26,11 +30,78 @@ function operate(operator, leftVariable, rightVariable){
             return add(leftVariable, rightVariable);
         case "-":
             return subtract(leftVariable, rightVariable);
-        case "x":
+        case "*":
             return multiply(leftVariable, rightVariable);
         case "/":
             return divide(leftVariable, rightVariable);
     }
 }
 
+function updateDisplay(changeTo = "0."){
+    display.textContent = changeTo;
+}
+
 // We'll add even listeners to the buttons so that they respond accordingly when theyre clicked
+document.querySelectorAll(".button").forEach( button =>
+    button.addEventListener("click", () => {
+        // The behavior will depend on the type of button pressed
+
+        // For numbers well concatenate the number pressed at the end of the number
+        if (button.classList.contains("number")) {
+            numbers[numberIndex] += button.textContent;
+            updateDisplay(numbers[0] + " " + operand + " " + numbers[1]);
+        }
+
+        // For operations we'll switch out the current operation 
+        if (button.classList.contains("operation")){
+            // We'll verify that the first part of the operation has been inputted
+
+            // If it hasnt we alert the user and leave
+            if (numbers[0] == ""){
+                alert("You need to input the first number first");
+                return;
+            }
+            // We also have to make a second number has been inputted
+            if (numbers[1] == "" && operand != ""){
+                alert("You need to input a second number");
+                return;
+            }
+
+            // Finally if both numbers have been inputted we evaluate the expression and take the result as the first number
+            if (numbers[0] != "" && numbers[1] != "") {
+                let result = operate(operand, parseInt(numbers[0]), parseInt(numbers[1])).toFixed(3);
+                numbers[0] = result;
+                numbers[1] = "";
+            }
+
+            operand = button.textContent;
+            numberIndex = 1;
+            updateDisplay(numbers[0] + " " + operand + " " + numbers[1]);
+        }
+
+        // For the clear button well empty out all the variables and reset the display
+        if (button.classList.contains("c")) {
+            numbers = ["", ""];
+            numberIndex = 0;
+            operand = "";
+            updateDisplay();
+        }
+
+        // If the button is the equals we evaluate the expression
+        if (button.classList.contains("eval")) {
+            // But first we check all the fields are filled
+            if (numbers[0] == "" && operand == "" && numbers[1] == ""){
+                alert("You need to input a valid expression first");
+                return;
+            }
+            let result = operate(operand, parseFloat(numbers[0]), parseFloat(numbers[1])).toFixed(3);
+            updateDisplay(result);
+        }
+
+        // Case for the backspace button
+        if (button.classList.contains("del")) {
+            // We will delete the rightmost character inputted
+            
+        }
+    })
+);
